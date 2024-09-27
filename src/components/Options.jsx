b35a51574a76e5caf48c43157b443db022b825ae
Sahil Suthar <sahil26dd@gmail.com>
2024-09-27 23:39:06 +0530

    handling answers

--- src/components/Options.jsx
+++ src/components/Options.jsx
@@ -1,9 +1,22 @@
 /* eslint-disable react/prop-types */
-export default function Options({ question }) {
+export default function Options({ question, dispatch, answer }) {
+  const hasAnswered = answer !== null;
+
   return (
     <div className="options">
-      {question.options.map((opt) => (
-        <button key={opt} className="btn btn-option">
+      {question.options.map((opt, index) => (
+        <button
+          key={opt}
+          className={`btn btn-option ${answer === index ? "answer" : ""} ${
+            hasAnswered
+              ? index === question.correctOption
+                ? "correct"
+                : "wrong"
+              : ""
+          }`}
+          disabled={hasAnswered}
+          onClick={() => dispatch({ type: "newAnswer", payload: index })}
+        >
           {opt}
         </button>
       ))}
