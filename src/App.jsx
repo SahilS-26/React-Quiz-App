e4678b7480877c09626998f08f5a3fc1999af0d0
Sahil Suthar <sahil26dd@gmail.com>
2024-09-27 18:14:03 +0530

    Start point of the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
-import DateCounter from "./DateCounter";
+import Header from "./Header.jsx";
+import Main from "./main.jsx";
 
 export default function App() {
   return (
-    <div>
-      <DateCounter />;
+    <div className="app">
+      <Header />
+      <Main>
+        <p>1/15</p>
+        <p>Question?</p>
+      </Main>
     </div>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
+// eslint-disable-next-line react/prop-types
+function Main({ children }) {
+  return (
+    <>
+      <main className="main">{children}</main>;
+    </>
+  );
+}
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+export default Main;
