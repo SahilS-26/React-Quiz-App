e03d065ec9211a8c7bd40dd8701ced062cb6dd81
Sahil Suthar <sahil26dd@gmail.com>
2024-09-28 14:04:27 +0530

    feat: point update fixed

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ const initialState = {
   status: "loading",
   index: 0,
   answer: null,
+  points: 0,
 };
 
 function reducer(state, action) {
@@ -36,11 +37,18 @@ function reducer(state, action) {
         status: "active",
       };
 
-    case "newAnswer":
+    case "newAnswer": {
+      const question = state.questions.at(state.index);
+
       return {
         ...state,
         answer: action.payload,
+        points:
+          action.payload === question.correctOption
+            ? state.points + question.points
+            : state.points,
       };
+    }
 
     default:
       throw new Error("Action Unknown");
